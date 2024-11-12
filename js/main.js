d6b33add0047d0beed5abeba53c44f651febead2
Nurmuhammad0422 <bnurmuhammad0422@gmail.com>
2024-11-12 18:41:44 +0500

    Project end

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-1-Vazifa
+1 - Vazifa;
 // 1-.
 const arr = [
   {
@@ -74,59 +74,59 @@ console.log(collectNumbers());
 // 2-VAZIFA
 // 1-.
 let person = {
-    name: "Abdulloh",
-    age: 19,
-    job: "Logistic company",
-    interest: "game",
-    hobby: "sleep",
-    number: "+998944683325",
-    username: "AU"
+  name: "Abdulloh",
+  age: 19,
+  job: "Logistic company",
+  interest: "game",
+  hobby: "sleep",
+  number: "+998944683325",
+  username: "AU",
 };
 for (let key in person) {
-    console.log(`${key} - ${person[key]}`);
+  console.log(`${key} - ${person[key]}`);
 }
 // 2-.
 const person1 = {
-    name: "Abdulloh",
-    age: 19
+  name: "Abdulloh",
+  age: 19,
 };
 
 const person2 = {
-    hobby: "Volleybol",
-    interest: "Sleep"
+  hobby: "Volleybol",
+  interest: "Sleep",
 };
 
 const mergedPerson = Object.assign({}, person1, person2);
 console.log(mergedPerson);
 // 3-.
 function addOneToEnd(arr) {
-    const lastValue = arr[arr.length - 1];
-    arr.push(lastValue + 1);
-    return arr;
+  const lastValue = arr[arr.length - 1];
+  arr.push(lastValue + 1);
+  return arr;
 }
 
 const number1 = [1, 2, 3, 4];
 console.log(addOneToEnd(number1));
 // 4-.
 function processArray(arr) {
-    let sum = arr.reduce((acc, num) => acc + num, 0);
-    if (arr[0] % 2 === 0) {
-        arr.unshift(sum);
-    } else {
-        arr.push(sum);
-    }
-    return arr;
+  let sum = arr.reduce((acc, num) => acc + num, 0);
+  if (arr[0] % 2 === 0) {
+    arr.unshift(sum);
+  } else {
+    arr.push(sum);
+  }
+  return arr;
 }
 
 const number = [2, 3, 4];
 console.log(processArray(number));
 // 5-.
 function swapFirstAndLast(arr) {
-    const first = arr[0];
-    const last = arr[arr.length - 1];
-    arr[0] = last;
-    arr[arr.length - 1] = first;
-    return arr;
+  const first = arr[0];
+  const last = arr[arr.length - 1];
+  arr[0] = last;
+  arr[arr.length - 1] = first;
+  return arr;
 }
 
 const number2 = [1, 2, 3, 4, 5];
@@ -159,16 +159,16 @@ function createArrayBySize() {
 createArrayBySize();
 // 2-.
 function uniqueArray(arr1) {
-    return [...new Set(arr1)];
+  return [...new Set(arr1)];
 }
 
 const arr1 = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5];
 console.log(uniqueArray(arr1));
 // 3-.
 function findIndexInArray(arr) {
-    const num = parseInt(prompt("Sonni kiriting:"), 10);
-    const index = arr.indexOf(num);
-    return index !== -1 ? index : "Son topilmadi";
+  const num = parseInt(prompt("Sonni kiriting:"), 10);
+  const index = arr.indexOf(num);
+  return index !== -1 ? index : "Son topilmadi";
 }
 
 const arr2 = [88, 75, 342, 21, 45, 67];
